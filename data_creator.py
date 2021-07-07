0386f6d68dd03886b2585ac4e62b8beaecf96534
tapish101 <85446029+tapish101@users.noreply.github.com>
2021-07-07 04:51:38 -0700

    Update data_creator.py

--- data_creator.py
+++ data_creator.py
@@ -1,21 +1,30 @@
+#this script creates the data of kills and save it to later compare with to find the kills
+
 import pyscreenshot
 import os
+#to install PIL run "pip install pillow" in the terminal
 from PIL import ImageEnhance
 
+#creating the folders to save data
 try:
     os.mkdir("data/img")
     os.mkdir("data/files")
 except FileExistsError:
     print("\nfolders already exist")
 
-
+#change these values with those you got from coordinate_finder.py script also change in the_screamer.py
 x1=101
 y1=110
 
 x2=126
 y2=134
 
-x=0
+#the factor holdes the val by which factor you want to contrast the image
+#and the thresh hold the threshold to Black & White the image if you change them here remember to change them also in the_screamer.py script
+factor=2
+thresh = 70
+
+x=0  #don't change it
 
 while(True):
     try:
@@ -25,10 +34,11 @@ while(True):
 
 
     if n==1:
+        #take the screenshot and crop it to size of given coordinates
         image_real = pyscreenshot.grab()
         img = image_real.crop((x1,y1,x2,y2)) 
         
-
+        #converting image to negative
         for i in range(0, img.size[0]-1):
 
             for j in range(0, img.size[1]-1):
@@ -41,11 +51,12 @@ while(True):
                 bluePixel   = 255 - pixelColorVals[2];
 
                 img.putpixel((i,j),(redPixel, greenPixel, bluePixel));
-
+        
+        #making it a bit more contrasy
         img_contr_obj=ImageEnhance.Contrast(img)
-        factor=2
         e_img=img_contr_obj.enhance(factor)
-        thresh = 70
+        
+        #making it one bit B&W and then saving it
         fn = lambda x : 255 if x > thresh else 0
         r = e_img.convert('L').point(fn, mode='1')
         r.save("data/img/"+ str(x)+".jpeg")
